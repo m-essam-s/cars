a2b54cb77583e0c2dbd990c27ebd09fb74d1cf9d
m-essam-s <sir.m.essam.g@gmail.com>
2024-05-30 12:57:45 +0300

    Enhancements for theams files

--- lib/themes/theme.dart
+++ lib/themes/theme.dart
@@ -1,32 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
-import 'package:flutter/widgets.dart';
 
-final ThemeData lightTheme = ThemeData(
-  primarySwatch: Colors.blue,
-  hintColor: Colors.amber,
-  textTheme: const TextTheme(
-    displayLarge: TextStyle(fontSize: 72.0, fontWeight: FontWeight.bold),
-    displayMedium: TextStyle(fontSize: 36.0, fontWeight: FontWeight.bold),
-    displaySmall: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
-    titleLarge: TextStyle(fontSize: 36.0, fontStyle: FontStyle.italic),
-    titleMedium: TextStyle(fontSize: 24.0, fontStyle: FontStyle.italic),
-    titleSmall: TextStyle(fontSize: 18.0, fontStyle: FontStyle.italic),
-    headlineLarge: TextStyle(fontSize: 24.0, fontFamily: 'Hind'),
-    headlineMedium: TextStyle(fontSize: 20.0, fontFamily: 'Hind'),
-    headlineSmall: TextStyle(fontSize: 16.0, fontFamily: 'Hind'),
-    bodyLarge: TextStyle(fontSize: 18.0, fontFamily: 'Hind'),
-    bodyMedium: TextStyle(fontSize: 14.0, fontFamily: 'Hind'),
-    bodySmall: TextStyle(fontSize: 12.0, fontFamily: 'Hind'),
-  ),
-  buttonTheme: const ButtonThemeData(
-    buttonColor: Colors.blue,
-    textTheme: ButtonTextTheme.primary,
-  ),
-);
+final ThemeData lightTheme = ThemeData.light();
 
-final ThemeData darkTheme = ThemeData(
-  brightness: Brightness.dark,
-  primarySwatch: Colors.blue,
-  hintColor: Colors.amber,
-);
+final ThemeData darkTheme = ThemeData.dark();
